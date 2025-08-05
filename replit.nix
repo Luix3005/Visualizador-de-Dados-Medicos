2f8b5728e50603d10d8d55ec38bb2329e7da26fb
Luiz  Felipe <45219000-felipearaujo108@users.noreply.replit.com>
2025-08-05 19:35:28 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python311
-    pkgs.python311Packages.pandas
-    pkgs.python311Packages.matplotlib
-    pkgs.python311Packages.seaborn
-    pkgs.python311Packages.numpy
-  ];
-}
